6dd1c96af4add58a8f0e2a3f3614086d3f491080
lehuuhieu-0805 <01643815296h@gmail.com>
2023-03-02 16:17:03 +0700

    feat: add redux toolkit for save user

---
+++ src/stores/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = null;
+
+const user = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    save: (state, action) => {
+      state = action.payload;
+      return state;
+    },
+    remove: (state) => {
+      state = initialState;
+      return state;
+    }
+  }
+});
+
+const { reducer, actions } = user;
+export const { save, remove } = actions;
+export default reducer;
